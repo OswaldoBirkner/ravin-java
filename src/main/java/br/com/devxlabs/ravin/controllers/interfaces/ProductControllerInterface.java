67bec19b98e250e194d69c4ae04498d800df4fe2
Oswaldo Birkner <98365827+OswaldoBirkner@users.noreply.github.com>
2023-08-23 18:35:37 -0300

    Entities criadas

--- src/main/java/br/com/devxlabs/ravin/controllers/interfaces/ProductControllerInterface.java
+++
@@ -1,7 +0,0 @@
-package br.com.devxlabs.ravin.controllers.interfaces;
-
-import br.com.devxlabs.ravin.entities.Product;
-
-public interface ProductControllerInterface  extends ControllerInterface<Product> {
-
-}
